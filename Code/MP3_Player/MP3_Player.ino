b27904198f63dd6de16bb50834de435b80eed5f8
Comyar Dehlavi <comyar2007@gmail,com>
2022-05-18 07:56:37 -0500

    Added Button Input

--- Code/MP3_Player/MP3_Player.ino
+++ Code/MP3_Player/MP3_Player.ino
@@ -5,19 +5,46 @@
 
 #define SD_ChipSelectPin 8
 #define led 10
+#define repeatBtn 4
+
+uint8_t skipBtns[2] = {5, 6}; // back, forward
 
 TMRpcm tmrpcm;
 Adafruit_SSD1306 display = Adafruit_SSD1306();
 
-int totalIndicies = 0;
-int currentIndex = 1;
+uint8_t totalIndicies = 0;
+uint8_t currentIndex = 1;
 
-bool displayed = false;
+bool repeat = false;
+
+void disp()
+{
+  if (currentIndex > totalIndicies)
+    currentIndex = 1;
+  else if (currentIndex == 0)
+    currentIndex = totalIndicies;
+
+  display.clearDisplay();
+  display.setTextSize(0.999);
+  display.setCursor(0, 0);
+  display.print("Now Playing");
+  if (repeat == true)
+  {
+    display.setTextSize(0.999);
+    display.setCursor(85, 0);
+    display.print("R");
+    display.display();
+  }
+  display.setCursor(105, 0);
+  display.print(String(currentIndex) + "/" + String(totalIndicies));
+  display.setTextSize(3.5);
+  display.setCursor(0, 12);
+  display.print("Audio" + String(currentIndex));
+  display.display();
+}
 
 void setup()
 {
-  Serial.begin(9600);
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
   display.setTextColor(WHITE);
   display.setTextSize(2.99999);
@@ -27,8 +54,13 @@ void setup()
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 
+  pinMode(repeatBtn, INPUT);
+  pinMode(skipBtns[0], INPUT);
+  pinMode(skipBtns[1], INPUT);
+
   if (!SD.begin(SD_ChipSelectPin))
   {
+    bool displayed = false;
     while (!SD.begin(SD_ChipSelectPin))
     {
       if (displayed == false)
@@ -39,7 +71,6 @@ void setup()
       }
     }
   }
-  displayed = false;
   display.clearDisplay();
   display.display();
 
@@ -63,31 +94,45 @@ void loop()
   {
     if (tmrpcm.isPlaying())
     {
-      // Do input handing ; song skipping, repeat, etc...
+      delay(1500);
+      if (digitalRead(repeatBtn) == HIGH) // repeat
+      {
+        repeat = !repeat;
+
+        if (repeat == true)
+        {
+          currentIndex--;
+          disp();
+        }
+        else
+        {
+          disp();
+          currentIndex++;
+        }
+      }
+
+      else if (digitalRead(5) == HIGH && repeat == false) // backwards
+      {
+        currentIndex -= 2;
+        tmrpcm.stopPlayback();
+      }
+      else if (digitalRead(6) == HIGH && repeat == false) // forwards
+        tmrpcm.stopPlayback();
     }
+
     else
     {
-      if (currentIndex > totalIndicies)
-        currentIndex = 1;
 
       digitalWrite(led, LOW);
 
-      display.clearDisplay();
-      display.setTextSize(0.999);
-      display.setCursor(0, 0);
-      display.print("Now Playing");
-      display.setCursor(105, 0);
-      display.print(String(currentIndex) + "/" + String(totalIndicies));
-      display.setTextSize(3.5);
-      display.setCursor(0, 12);
-      display.print("Audio" + String(currentIndex));
-      display.display();
+      disp();
 
       String file = "Audio" + String(currentIndex) + ".wav";
       tmrpcm.play((char *)file.c_str());
 
       digitalWrite(led, HIGH);
-      currentIndex++;
+      if (repeat == false)
+        currentIndex++;
     }
   }
 }
